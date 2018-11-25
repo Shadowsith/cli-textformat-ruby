4e0aae1a504108c35349f446300d4d05fedefcf9
Shadowsith <shadowsith94@gmail.com>
2018-11-25 02:15:30 +0100

    adding some test messages

--- test.rb
+++ test.rb
@@ -7,6 +7,8 @@ puts format.red("test")
 puts format.green("test")
 puts format.yellow("test")
 puts format.blue("test")
+puts format.magenta("test")
+puts format.cyan("test")
 puts format.white("test")
 puts format.black("test")
 puts
@@ -16,11 +18,13 @@ puts format.red_bg("test")
 puts format.green_bg("test")
 puts format.yellow_bg("test")
 puts format.blue_bg("test")
-puts format.white_bg("test")
+puts format.magenta_bg("test")
+puts format.cyan_bg("test")
+puts format.white_bg(format.black("test"))
 puts format.black_bg("test")
 puts
 
-puts format.red("Error Message:", true)
+puts format.red("Error message:", true)
 puts "Error 1 at line 2"
 puts "Error 2 at line 40"
 puts format.disable_multiline 
